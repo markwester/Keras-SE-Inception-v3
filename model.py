005a6a99fb64b09d6ccf8a07500f9b01388ef9b6
Mark <34436787+markwester@users.noreply.github.com>
2018-10-15 15:30:23 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 def build_model(out_dims, input_shape=(224, 224, 3)):
     inputs_dim = Input(input_shape)
     x = Lambda(lambda x: x / 255.0)(inputs_dim) #在模型里进行归一化预处理
- 
+    #InceptionV3部分
     x = InceptionV3(include_top=False,
                 weights='imagenet',
                 input_tensor=None,
                 input_shape=(224, 224, 3),
                 pooling=max)(x)
- 
+    #SENet部分
     squeeze = GlobalAveragePooling2D()(x)
  
     excitation = Dense(units=2048 // 11)(squeeze)
@@ -17,7 +17,7 @@ def build_model(out_dims, input_shape=(224, 224, 3)):
     excitation = Reshape((1, 1, 2048))(excitation)
  
     scale = multiply([x, excitation])
- 
+    
     x = GlobalAveragePooling2D()(scale)
     dp_1 = Dropout(0.6)(x)
     fc2 = Dense(out_dims)(dp_1)
